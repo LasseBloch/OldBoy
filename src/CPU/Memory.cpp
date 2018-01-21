fb2e2acfd93452f651eaa89a9b9ab372e3dc8de2
LasseBloch <lassebloch@gmail.com>
2018-01-21 20:15:14 +0100

    Added boot sequence code

--- src/CPU/Memory.cpp
+++ src/CPU/Memory.cpp
@@ -2,4 +2,33 @@
 // Created by Lasse Lauritsen on 21/01/2018.
 //
 
+#include <fstream>
+#include <assert.h>
+#include <iostream>
 #include "Memory.h"
+
+void Memory::loadCartridge(std::string romPath)
+{
+    std::ifstream myFile;
+
+    myFile.open(romPath, std::ios::in | std::ios::binary);
+
+    uint8_t byte;
+    uint16_t currentAddr = cartridgeStart_;
+    while(myFile.good())
+    {
+        if (currentAddr <= cartridgeEnd_) {
+            byte = myFile.get();
+            mem_[currentAddr] = byte;
+            currentAddr++;
+        } 
+        else
+        {
+            std::cout << "Ups GameBoy Cartridges are only supported to hold 32kB" << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "Done" << std::endl;
+
+}
